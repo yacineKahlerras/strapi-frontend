d11b5ae78d365d60e17e7302ee4f73af5feb7569
Yacine Kahlerras <kahlerasse@gmail.com>
2024-12-09 16:22:27 +0100

    other updates

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -4,11 +4,12 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function MainSection({ data }: { data: ArticleType[] }) {
-  const [activeArticle, setActiveArticle] = useState(0);
+  const [activeArticle] = useState(0);
   const currentArticle = data[activeArticle];
 
   return (
     <article className="mt-36">
+      {/* left section */}
       <div>
         <div>
           <h2>{currentArticle.title}</h2>
@@ -23,6 +24,9 @@ export default function MainSection({ data }: { data: ArticleType[] }) {
           />
         </div>
       </div>
+
+      {/* right section */}
+      <div></div>
     </article>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["https://my-strapi-9n2k.onrender.com"], // Add the domain from which you're fetching images
+    domains: ["my-strapi-9n2k.onrender.com"], // Add the domain from which you're fetching images
   },
 };
 
