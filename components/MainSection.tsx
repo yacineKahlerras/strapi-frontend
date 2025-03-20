e299c40b8d2ebed8809859e05689b4715d5d2861
Yacine Kahlerras <kahlerasse@gmail.com>
2025-03-20 21:44:52 +0100

    added size limits

--- components/MainSection.tsx
+++ components/MainSection.tsx
@@ -4,58 +4,62 @@ const stars = ["full", "full", "full", "full", "half"];
 
 export default function MainSection() {
   return (
-    <div className="w-full px-8 pl-20 pt-14 bg-black h-[652px] overflow-hidden flex flex-col items-start text-white relative rounded-b-[2.5rem]">
-      {/* header */}
-      <h1 className="text-[52px] font-bold font-helvetica pt-12 z-20">
-        Fly stress-free, <br />
-        We handle your bags
-      </h1>
+    <div className="flex justify-center items-start overflow-hidden relative rounded-b-[2.5rem] h-[652px]">
+      <div className="w-full h-full flex-shrink bg-black"></div>
+      <div className="w-[min(100%,90rem)] h-full flex-shrink-0 px-8 pl-20 pt-14 bg-black flex flex-col items-start text-white relative">
+        {/* header */}
+        <h1 className="text-[52px] font-bold font-helvetica pt-12 z-20">
+          Fly stress-free, <br />
+          We handle your bags
+        </h1>
 
-      {/* ratings */}
-      <div className="flex items-center gap-5 mt-3 pb-7 font-semibold">
-        <span>
-          4.9/5{" "}
-          <small className="text-base opacity-80 font-light">Rating</small>
+        {/* ratings */}
+        <div className="flex items-center gap-5 mt-3 pb-7 font-semibold">
+          <span>
+            4.9/5{" "}
+            <small className="text-base opacity-80 font-light">Rating</small>
+          </span>
+          <ul className="flex">
+            {stars.map((s, indx) => (
+              <li key={indx}>
+                <Image
+                  width={15.27}
+                  height={15.27}
+                  alt="star"
+                  src={`/${s}-star.svg`}
+                />
+              </li>
+            ))}
+          </ul>
+          <span>Trustpilot</span>
+        </div>
+
+        {/* cto */}
+        <button className="bg-[#FDCD00] hover:bg-[#fdcf00e1] flex items-center justify-center text-lg font-bold gap-5 px-[40px] py-[18px] rounded-xl text-black mb-6">
+          Book now
+          <Image width={34} height={34} alt="star" src="/arrow-right.svg" />
+        </button>
+        <span className="opacity-70 font-semibold text-lg pl-8">
+          Starting from $25
         </span>
-        <ul className="flex">
-          {stars.map((s, indx) => (
-            <li key={indx}>
-              <Image
-                width={15.27}
-                height={15.27}
-                alt="star"
-                src={`/${s}-star.svg`}
-              />
-            </li>
-          ))}
-        </ul>
-        <span>Trustpilot</span>
-      </div>
 
-      {/* cto */}
-      <button className="bg-[#FDCD00] hover:bg-[#fdcf00e1] flex items-center justify-center text-lg font-bold gap-5 px-[40px] py-[18px] rounded-xl text-black mb-6">
-        Book now
-        <Image width={34} height={34} alt="star" src="/arrow-right.svg" />
-      </button>
-      <span className="opacity-70 font-semibold text-lg pl-8">
-        Starting from $25
-      </span>
-
-      <div className="bg-[#FDCD00] absolute right-0 top-0 w-[39%] h-full flex items-end justify-start pl-32 pt-9">
-        <Image width={391} height={590} src="/woman.png" alt="woman" />
-        <Image
-          className="absolute left-0 bottom-0 xl:top-[2rem] -translate-x-1/2"
-          width={279}
-          height={436}
-          src="/case.png"
-          alt="case"
-        />
+        <div className="bg-[#FDCD00] absolute right-0 top-0 w-[39%] h-full flex items-end justify-start pl-32 pt-9">
+          <Image width={391} height={590} src="/woman.png" alt="woman" />
+          <Image
+            className="absolute left-0 bottom-0 xl:top-[2rem] -translate-x-1/2"
+            width={279}
+            height={436}
+            src="/case.png"
+            alt="case"
+          />
+        </div>
+        <div className="opacity-[0.035] bg-white w-16 aspect-square absolute bottom-0 left-[40%]"></div>
+        <div className="opacity-[0.035] bg-white w-16 aspect-square absolute bottom-[3rem] left-[1.5rem]"></div>
+        <div className="opacity-[0.035] bg-white w-16 aspect-square absolute top-[5rem] left-[3.5rem]"></div>
+        <div className="opacity-[0.035] bg-white w-16 aspect-square absolute top-[0rem] left-[20%]"></div>
+        <div className="opacity-[0.035] bg-white w-32 h-16 absolute bottom-[0rem] left-[10%]"></div>
       </div>
-      <div className="opacity-[0.035] bg-white w-16 aspect-square absolute bottom-0 left-[40%]"></div>
-      <div className="opacity-[0.035] bg-white w-16 aspect-square absolute bottom-[3rem] left-[1.5rem]"></div>
-      <div className="opacity-[0.035] bg-white w-16 aspect-square absolute top-[5rem] left-[3.5rem]"></div>
-      <div className="opacity-[0.035] bg-white w-16 aspect-square absolute top-[0rem] left-[20%]"></div>
-      <div className="opacity-[0.035] bg-white w-32 h-16 absolute bottom-[0rem] left-[10%]"></div>
+      <div className="w-full h-full flex-shrink bg-[#FDCD00]"></div>
     </div>
   );
 }
--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -17,30 +17,32 @@ const navLinks = [
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center justify-between px-14 py-4 font-semibold">
-      <Image src="/logo.svg" width={145} height={23} alt="logo" />
-      <ul className="flex items-center justify-center gap-9 xl:pl-[15rem]">
-        {navLinks.map(({ text, url }) => (
-          <li key={text}>
-            <a
-              className="hover:opacity-80 hover:underline hover:underline-offset-8"
-              href={url}
-            >
-              {text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <div></div>
-      <div className="flex items-center gap-6">
-        <button className="flex items-center gap-2">
-          <Image width={20} height={15} alt="flag" src="/british-flag.svg" />
-          <span>EN</span>
-          <Image width={16} height={16} alt="flag" src="/down-arrow.svg" />
-        </button>
-        <button className="font-bold border-2 border-black rounded-lg px-4 py-2 hover:opacity-80">
-          For Business
-        </button>
+    <nav className="flex items-center justify-center px-14 py-4 font-semibold">
+      <div className="w-[min(100%,90rem)] flex items-center justify-between">
+        <Image src="/logo.svg" width={145} height={23} alt="logo" />
+        <ul className="flex items-center justify-center gap-9 xl:pl-[15rem]">
+          {navLinks.map(({ text, url }) => (
+            <li key={text}>
+              <a
+                className="hover:opacity-80 hover:underline hover:underline-offset-8"
+                href={url}
+              >
+                {text}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div></div>
+        <div className="flex items-center gap-6">
+          <button className="flex items-center gap-2">
+            <Image width={20} height={15} alt="flag" src="/british-flag.svg" />
+            <span>EN</span>
+            <Image width={16} height={16} alt="flag" src="/down-arrow.svg" />
+          </button>
+          <button className="font-bold border-2 border-black rounded-lg px-4 py-2 hover:opacity-80">
+            For Business
+          </button>
+        </div>
       </div>
     </nav>
   );
